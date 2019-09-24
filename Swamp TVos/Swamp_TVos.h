aca344fc515f8e52b6f117d9ee354fcbd80a8eae
mystis <b.kuznecov@gmail.com>
2019-09-24 12:38:36 +0300

    fix

---
+++ Swamp TVos/Swamp_TVos.h
@@ -0,0 +1,19 @@
+//
+//  Swamp_TVos.h
+//  Swamp TVos
+//
+//  Created by Борис Кузнецов on 24.09.2019.
+//  Copyright © 2019 iscriptology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Swamp_TVos.
+FOUNDATION_EXPORT double Swamp_TVosVersionNumber;
+
+//! Project version string for Swamp_TVos.
+FOUNDATION_EXPORT const unsigned char Swamp_TVosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swamp_TVos/PublicHeader.h>
+
+
